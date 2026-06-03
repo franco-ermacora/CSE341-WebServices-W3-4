879745ba17210016ce324e0e61cc429ba372bdcf
Franco Ermacora <ermacora.fran@gmail.com>
2026-06-03 00:59:52 -0300

    W04 Project: Project 2 Part 2 (Authentication) V.0.4

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,23 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+// RUTA LOGIN
 router.get('/login', passport.authenticate('github'), (req, res) => {});
 
+// RUTA CALLBACK (Esto lo necesita GitHub)
 router.get('/github/callback', passport.authenticate('github', {
-  failureRedirect: '/api-docs',
-  session: true
+    failureRedirect: '/api-docs',
+    sessionSuccessRedirect: '/api-docs' // Redirige a donde está tu Swagger
 }), (req, res) => {
-  req.session.user = req.user;
-  res.redirect('/');
+    res.status(200).send('Logged in');
 });
 
-router.get('/logout', (req, res, next) => {
-  req.logout((err) => {
-    if (err) { return next(err); }
-    res.redirect('/');
-  });
+// RUTA LOGOUT
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/'); // Vuelve al inicio
+    });
 });
 
 module.exports = router;
